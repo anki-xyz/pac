a0faa7bcb6e12fdf32606b8baf262bac73e7c944
anki-xyz <me@anki.xyz>
2021-10-10 22:45:25 +0200

    v

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(name='pac',
-      version='0.1',
+      version='0.1a',
       author='Andreas Kist',
       author_email='me@anki.xyz',
       packages=find_packages(),
